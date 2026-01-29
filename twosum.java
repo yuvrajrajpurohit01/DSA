ff13fd13f6dae1cb4c6652fcc4d392ac8582f2a3
YUVRAJ <yuvrajrajpurohitt@gmail.com>
2026-01-29 21:04:56 +0530

    Initial commit

---
+++ twosum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (i=0;i<nums.length;i++){
+            for(j=i+1;j<nums.length;j++){
+                if(arr[i]+arr[j]=target){
+                    return int[] i,j;
+                }
+            }
+        }
+    }
+}
