dcf17345500f25b61efcd1114bf475093f8a270b
YUVRAJ <yuvrajrajpurohitt@gmail.com>
2026-01-29 21:56:38 +0530

    Two Sum Code

--- Leetcode/twosum.java
+++ Leetcode/twosum.java
@@ -1,12 +1,12 @@
-package Leetcode;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (i=0;i<nums.length;i++){
-            for(j=i+1;j<nums.length;j++){
-                if(arr[i]+arr[j]=target){
-                    return int[] i,j;
+        for (int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[] {i,j} ;
                 }
             }
         }
+      return new int[] {};
     }
-}
+};
