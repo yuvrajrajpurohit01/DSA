2b24a570b6df0dd849331a35448fbe0f6b8ddddd
YUVRAJ <yuvrajrajpurohitt@gmail.com>
2026-01-29 21:27:47 +0530

    code

--- Codeforces/watermelon.java
+++ Codeforces/watermelon.java
@@ -1,5 +1,14 @@
-package Codeforces;
-
+import java.util.Scanner;
+import java.util.*;
 public class watermelon {
     
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int w = sc.nextInt();
+        if((w>2) && (w%2==0)){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
 }
