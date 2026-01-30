0c212966e79fa238e2944e7c59c280fef4269f3a
YUVRAJ <yuvrajrajpurohitt@gmail.com>
2026-01-30 20:09:02 +0530

    Elephant code

---
+++ Codeforces/elephant.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class elephant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int steps = (n + 4) / 5 ;
+        System.out.println(steps);
+        
+    }   
+}
