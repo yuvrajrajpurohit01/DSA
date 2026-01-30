c1e46a509c747fd5a984574a05ac6fdb88ab05db
YUVRAJ <yuvrajrajpurohitt@gmail.com>
2026-01-30 21:27:18 +0530

    2nd day

---
+++ Leetcode/sqrt.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class sqrt {
+            public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int x= sc.nextInt();
+                int sqrt = (int)(Math.floor(Math.sqrt(x)));
+                
+
+                System.out.println(sqrt);
+
+
+            }
+
+}
