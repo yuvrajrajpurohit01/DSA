f868c90ed00cf5f9db869738b4d63eb80d4a5233
YUVRAJ <yuvrajrajpurohitt@gmail.com>
2026-02-12 21:39:02 +0530

    new add-ons

---
+++ Codeforces/team.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class team {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        for(int i = 0;i<n;i++){
+            int p = sc.nextInt();
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            if((p+v+t)>= 2){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
