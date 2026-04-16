72928a29d3258b73716f8799336587687af7c9f3
YUVRAJ <yuvrajrajpurohitt@gmail.com>
2026-04-16 11:34:25 +0530

    Consistency

--- Codeforces/vanyacube.java
+++ Codeforces/vanyacube.java
@@ -1,15 +1,18 @@
 import java.util.*;
+
 public class vanyacube {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if(n==1){System.out.println(n);}
-        else{
-            int count = 0;
-            for(int i = 1 ; i<n ; i = ){
-                if(i)
-            }
-            System.out.println(count);
+
+        int i = 1, count = 0;
+
+        while (n >= (i * (i + 1)) / 2) {
+            n -= (i * (i + 1)) / 2;
+            count++;
+            i++;
         }
+
+        System.out.println(count);
     }
 }
